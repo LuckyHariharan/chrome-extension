f9dea7b8bc2e66fa15146f95546476ff38e645bf
LuckyHariharan <lharihar@uwaterloo.ca>
2023-10-08 20:22:02 -0400

    save working version

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -243,12 +243,14 @@ const Popup = () => {
           ${result || 0}
         </span>
       </p>
-      <button
-        className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
-        onClick={handleBackToStage1}
-      >
-        Back to Input
-      </button>
+      <div className="py-32">
+        <button
+          className="bg-blue-500 text-white px-4 py-4 rounded hover:bg-blue-600 "
+          onClick={handleBackToStage1}
+        >
+          Back to Input
+        </button>
+      </div>
     </div>
   );
 
